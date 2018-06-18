2891fd5b4585d0decb8ac7775affb3e02b196f16
Matthew Mann <mattmann0916@gmail.com>
2018-06-18 12:11:27 +0300

    Music Player Version 1

---
+++ MusicPlayer/GenreButtonScreenViewController.swift
@@ -0,0 +1,68 @@
+//
+//  GenreButtonScreenViewController.swift
+//  MusicPlayer
+//
+//  Created by Matthew Mann on 6/17/18.
+//  Copyright © 2018 Matthew Mann. All rights reserved.
+//
+
+import UIKit
+import MediaPlayer
+
+class GenreButtonScreenViewController: UIViewController {
+    
+    var musicPlayer = MPMusicPlayerController.applicationMusicPlayer
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func genreButtonTapped(_ sender: UIButton) {
+        MPMediaLibrary.requestAuthorization { (status) in
+            if status == .authorized {
+                self.playGenre(genre: sender.currentTitle!)
+            }
+        }
+        
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        musicPlayer.stop()
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        musicPlayer.skipToNextItem()
+    }
+    
+    func playGenre(genre : String) {
+        musicPlayer.stop()
+        
+        let query = MPMediaQuery()
+        let predicate = MPMediaPropertyPredicate(value: genre, forProperty: MPMediaItemPropertyGenre)
+        
+        query.addFilterPredicate(predicate)
+        
+        musicPlayer.setQueue(with: query)
+        musicPlayer.shuffleMode = .songs
+        musicPlayer.play()
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
